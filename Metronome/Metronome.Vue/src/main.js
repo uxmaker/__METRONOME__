9cbd595a5b905345958866369f8eef216afed0dd
Reybun <simajchel@intechinfo.fr>
2020-01-20 09:17:03 +0100

    autocomplete

--- Metronome/Metronome.Vue/src/main.js
+++ Metronome/Metronome.Vue/src/main.js
@@ -3,7 +3,10 @@ import AuthService from './services/AuthService'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Autocomplete from '@trevoreyre/autocomplete-vue'
+import '@trevoreyre/autocomplete-vue/dist/style.css'
 
+Vue.use(Autocomplete)
 Vue.config.productionTip = false
 
 const main = async() => {
